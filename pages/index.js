32d6164fdd707abbac7e4bf1713b78eda3e0ba7d
Hiroki Nagai <jiityan8000@gmail.com>
2022-06-24 08:39:51 +0900

    トップページのスタイル変更

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,21 @@
 import Link from "next/link";
 import { client } from "../libs/client";
-import styled from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #262E45;
+  }
+  a {
+    color: #E6E6E6;
+  }
+`
 export default function Home({ blog }) {
 
   const ArticleList = styled.ul`
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
     width: 100%;
     max-width: 940px;
     margin: 0 auto;
@@ -20,13 +31,32 @@ export default function Home({ blog }) {
     border-bottom: 1px solid #f2f2f2;
   `
 
+  const ArticleItemLink = styled.a`
+    display: block;
+    width: 520px;
+    border: 2px solid #fff;
+    border-radius: 30px;
+    box-sizing: content-box;
+    filter: drop-shadow(0 3px 6px rgba(0, 0, 0, .16));
+    transition: 0.3s;
+  `
+
+  const ArticleItemName = styled.span`
+    display: flex;
+    align-items: center;
+    height: 75px;
+    padding: 0 30px;
+    font-size: 24px;
+  `
+
   return (
     <div>
+
       <ArticleList>
         {blog.map((blog) => (
           <ArticleItem key={blog.id}>
             <Link href={`/blog/${blog.id}`}>
-              <a>{blog.title}</a>
+              <ArticleItemLink><ArticleItemName>{blog.title}</ArticleItemName></ArticleItemLink>
             </Link>
           </ArticleItem>
         ))}
