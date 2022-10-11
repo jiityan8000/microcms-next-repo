06e5ebb1640c395eeb966c7c9de88a15c3026683
Hiroki Nagai <jiityan8000@gmail.com>
2022-10-11 10:00:08 +0900

    GA4のIDを直書きに変更

--- pages/_document.js
+++ pages/_document.js
@@ -12,7 +12,7 @@ const Document = () => {
                     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                    })(window,document,'script','dataLayer','${process.env.GTM_ID}');
+                    })(window,document,'script','dataLayer','GTM-B21P62H8VF');
                 `,
                     }}
                 />
@@ -22,7 +22,7 @@ const Document = () => {
                     dangerouslySetInnerHTML={{
                         __html: `            
                         <iframe
-                        src="https://www.googletagmanager.com/ns.html?id=${process.env.GTM_ID}"
+                        src="https://www.googletagmanager.com/ns.html?id=GTM-B21P62H8VF"
                         height="0"
                         width="0"
                         style="display:none;visibility:hidden"
