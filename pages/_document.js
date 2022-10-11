cfb2be1662e6ea2a42301cecfd87850706327a08
Hiroki Nagai <jiityan8000@gmail.com>
2022-10-12 08:32:09 +0900

    GTMのID修正

--- pages/_document.js
+++ pages/_document.js
@@ -12,7 +12,7 @@ const Document = () => {
                     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                    })(window,document,'script','dataLayer','GTM-B21P62H8VF');
+                    })(window,document,'script','dataLayer','GTM-MC3M58X');
                 `,
                     }}
                 />
@@ -22,7 +22,7 @@ const Document = () => {
                     dangerouslySetInnerHTML={{
                         __html: `            
                         <iframe
-                        src="https://www.googletagmanager.com/ns.html?id=GTM-B21P62H8VF"
+                        src="https://www.googletagmanager.com/ns.html?id=GTM-MC3M58X"
                         height="0"
                         width="0"
                         style="display:none;visibility:hidden"
